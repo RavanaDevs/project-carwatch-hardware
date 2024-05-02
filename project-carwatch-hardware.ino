493226b0ba9681cdf7b214d437f8816ffda93a45
Kavindu Sanjula <ravanadevs@gmail.com>
2024-05-02 12:00:43 +0530

    Hardware

--- project-carwatch-hardware.ino
+++ project-carwatch-hardware.ino
@@ -2,18 +2,28 @@
 #include <WiFiClient.h>
 #include <WebSocketsClient.h>
 #include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
 #define WIFI_SSID "sanju-server"
 #define WIFI_PASSWORD "12345678"
 #define WIFI_ATTEMPT_COUNT 10
 
+enum SystemState {
+  STATE_TRANSMITTING,
+  STATE_DEBUG,
+};
+
+SystemState currntState = STATE_DEBUG;
+
+SoftwareSerial btSerial(D4, D3);  //TX RX
 WebSocketsClient webSocket;
 WiFiClient wifiClient;
 StaticJsonDocument<1024> doc;
 
 void setup() {
   Serial.begin(9600);
-  delay(1000);
+  btSerial.begin(9600);
+  delay(5000);
 
   Serial.println("Connecting");
   connectToWIFI();
@@ -25,9 +35,52 @@ void setup() {
   webSocket.setReconnectInterval(5000);
 }
 
+
+String input = "";
+String data = "";
+byte lock = 0;
+bool btSendLock = true;
+
+static uint32_t lastTime = millis();
+
 void loop() {
   webSocket.loop();
+
+  switch (currntState) {
+    case STATE_TRANSMITTING:
+      {
+        if (millis() - lastTime > 5000) {
+          lastTime = millis();
+          btSerial.println("atz");
+          readELM("atz");
+        }
+        break;
+      }
+    case STATE_DEBUG:
+      {
+        readELM(input);
+        break;
+      }
+  }
 }
+
+void readELM(String key) {
+  while (btSerial.available() > 0) {
+    char c = btSerial.read();
+    if (c == '>') {
+      doc.clear();
+      doc[key] = data;
+      String jsonData;
+      serializeJson(doc, jsonData);fdg
+      webSocket.sendTXT(jsonData);
+      data = "";
+      lock--;
+      break;
+    }
+    data += c;
+  }
+}
+
 
 void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
   switch (type) {
@@ -36,7 +89,26 @@ void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
     case WStype_CONNECTED:
       break;
     case WStype_TEXT:
-      Serial.println(String((char*)payload));
+      String msg = String((char*)payload);
+
+      if (msg == "bt-unlock") {
+        btSendLock = false;
+        return;
+      } else if (msg == "debug on") {
+        currntState = STATE_DEBUG;
+      } else if (msg == "debug off") {
+        currntState = STATE_TRANSMITTING;
+      }
+
+      if (!btSendLock && lock == 0) {
+        btSendLock = true;
+        lock++;
+
+        if (currntState == STATE_DEBUG) {
+          input = msg;
+          btSerial.println(msg);
+        }
+      }
       break;
   }
 }
