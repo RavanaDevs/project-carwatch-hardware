ba34e9d1b5df2eb119c0d2a7369a4cc5be5d8823
Kavindu Sanjula <ravanadevs@gmail.com>
2024-05-03 10:17:53 +0530

    remove unwanted codes

--- project-carwatch-hardware.ino
+++ project-carwatch-hardware.ino
@@ -8,13 +8,8 @@
 #define WIFI_PASSWORD "12345678"
 #define WIFI_ATTEMPT_COUNT 10
 
-enum SystemState {
-  STATE_SETUP,
-  STATE_TRANSMITTING,
-  STATE_DEBUG,
-};
+#define CMD_COUNT 3
 
-SystemState currntState = STATE_SETUP;
 
 SoftwareSerial btSerial(D4, D3);  //TX RX
 WebSocketsClient webSocket;
@@ -37,65 +32,15 @@ void setup() {
 }
 
 
-String input = "";
 String data = "";
-
-byte readLock = 1;
-
-byte lock = 0;
-bool btSendLock = true;
-
 static uint32_t lastTime = millis();
 
 void loop() {
   webSocket.loop();
 
-  switch (currntState) {
-    case STATE_SETUP:
-      {
-        btSerial.println("ATSP0");
-        delay(1000);
-        currntState = STATE_TRANSMITTING;
-      }
-    case STATE_TRANSMITTING:
-      {
-        // if (millis() - lastTime > 2000) {
-        //   lastTime = millis();
-        //   btSerial.println("ATZ");
-        //   delay(50);
-        //   readELM("ATZ");
-        // }
-        btSerial.println("010c");
-        readELM("010c");
 
-        btSerial.println("0111");
-        readELM("0111");
-
-        break;
-      }
-    case STATE_DEBUG:
-      {
-        if(readLock == 0){
-          readLock++;
-          readELM(input);
-        }
-        break;
-      }
-  }
 }
 
-void readELM(String key) {
-  while (btSerial.available() > 0) {
-    char c = btSerial.read();
-    if (c == '>') {
-      sendData(key, data);
-      data = "";
-      lock--;
-      break;
-    }
-    data += c;
-  }
-}
 
 void sendData(String key, String data) {
   doc.clear();
@@ -115,33 +60,6 @@ void webSocketEvent(WStype_t type, uint8_t* payload, size_t length) {
     case WStype_CONNECTED:
       break;
     case WStype_TEXT:
-      String msg = String((char*)payload);
-
-      if (msg == "bt-unlock") {
-        btSendLock = false;
-        return;
-      } else if (msg == "debug on") {
-        currntState = STATE_DEBUG;
-        return;
-      } else if (msg == "debug off") {
-        currntState = STATE_SETUP;
-        return;
-      }
-
-      if(currntState != STATE_DEBUG){
-        return;
-      }
-
-      Serial.println(msg);
-      if (!btSendLock && lock == 0) {
-        btSendLock = true;
-        lock++;
-        input = msg;
-        btSerial.println(msg);
-        delay(50);
-        readLock--;
-
-      }
       break;
   }
 }
